85bbc5fe931334a3911bdfe198d47d5478f6286b
penn <penn201500@gmail.com>
2017-07-24 05:42:34 +0000

    updatge add_two_sums.py and add leetcode-python.pdf

--- add_two_nums.py
+++ add_two_nums.py
@@ -13,10 +13,15 @@ def get_sum(arr1,arr2):
     arr3 = [0]*len_arr3
     print("init",arr3)
 
+    arr1 = arr1 + [0]*(len_arr3 - len(arr1))
+    arr2 = arr2 + [0]*(len_arr3 - len(arr2))
     for i in range(len_arr3):
-        if arr1[1]+arr2[i] >=10:
+        if arr1[i]+arr2[i] >=10:
             arr3[i] += arr1[i]+arr2[i]-10
-            arr3[i+1] = 1
+            if (i+1)<=len_arr3-1:
+                arr3[i+1] = 1
+            else:
+                break
         else:
             arr3[i] += arr1[i]+arr2[i]
     print(arr3)
@@ -24,5 +29,5 @@ def get_sum(arr1,arr2):
 
 if __name__ == '__main__':
     arr1 = [1,2,3]
-    arr2 = [4,5,6]
+    arr2 = [4,5,6,7,8]
     get_sum(arr1,arr2)
